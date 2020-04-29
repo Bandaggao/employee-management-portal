34ed80ff05b8640d34766cdf8296467f039fdc5f
Bandaggao <ban_daggao@questronix.com.ph>
2020-04-29 20:21:12 +0800

    setup and integrate backend

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <qNavbar></qNavbar>
+      <!-- <router-view/> -->
+      <!-- <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link> -->
+    </div>
+    <router-view/>
+  </div>
+</template>
+<script>
+import qNavbar from './components/nav/Navbar.vue'
+
+export default {
+  components: {
+    qNavbar
+  }
+}
+</script>>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+
+    <Employees msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Employees from '@/components/Employees.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Employees
+  }
+}
+</script>
